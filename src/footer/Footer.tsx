a495bda6c0861b778521528bac1b660188fb478b
Serj-Y <newlife101101@gmail.com>
2023-07-04 21:28:20 +0300

    add variable scss

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -7,6 +7,10 @@ import ReduxLogo from "../common/assets/image/Redux.png"
 import TypeScriptLogo from "../common/assets/image/TypeScript.png"
 import { SkillBlockConstructor } from "../common/components/skillConstructor/SkillBlockConstructor";
 import { TitleConstructor } from "../common/components/titleConstructor/TitleConstructor";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGit, faGithub, faJs, faReact, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faMobileScreenButton } from "@fortawesome/free-solid-svg-icons";
+
 
 type PropsType = {
 
@@ -30,6 +34,12 @@ export const Footer: React.FC<PropsType> = (props) => {
                 <SkillBlockConstructor
                     img={TypeScriptLogo} alt={"TypeScriptLogo"} propImgStyle={styles.Img} propBlockStyle={styles.Block}
                 />
+                {/* <FontAwesomeIcon icon={faReact} size="3x" />
+                <FontAwesomeIcon icon={faJs} />
+                <FontAwesomeIcon icon={faGit} />
+                <FontAwesomeIcon icon={faGithub} />
+                <FontAwesomeIcon icon={faYoutube} />
+                <FontAwesomeIcon icon={faMobileScreenButton} /> */}
             </div>
             <h6>© 2023 Serj_Y. All rights reserved.</h6>
         </div>
