264a12ea152cda3cdecc625992455e80befed0ac
Serj-Y <newlife101101@gmail.com>
2023-07-19 19:13:02 +0300

    upgrades in component Contact Us

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,4 +1,4 @@
-import React  from "react";
+import React from "react";
 import styles from "./Contacts.module.scss"
 import { TitleConstructor } from "../common/components/titleConstructor/TitleConstructor";
 import { Fade } from "react-awesome-reveal";
@@ -9,16 +9,13 @@ type PropsType = {
 
 }
 
-
-
-
 export const Contacts: React.FC<PropsType> = (props) => {
     return (
         <div id="Contacts" className={styles.Contacts}>
-             <TitleConstructor Title={"Contacts"}  />
+            <TitleConstructor Title={"Contacts"} />
             <div className={styles.Container} >
                 <Fade direction="up" >
-                   <ContactsUs/>
+                    <ContactsUs />
                 </Fade>
             </div>
         </div>
--- src/contacts/ContactsUs.tsx
+++ src/contacts/ContactsUs.tsx
@@ -1,32 +1,67 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./Contacts.module.scss"
 import emailjs from '@emailjs/browser';
 
-type PropsType = {
 
-}
 
-export const ContactsUs: React.FC<PropsType> = (props) => {
+export const ContactsUs = () => {
 
-    const form: any = useRef();
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [text, setText] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+    const [formStatus, setFormStatus] = useState("Submit")
+    const refOfForm: any = useRef();
 
-    const sendEmail = (e: any) => {
+    const resetFormValue = () => {
+        setName("")
+        setEmail("")
+        setText("")
+    }
+
+    const changeFormStatusTimeOut = () => {
+        setFormStatus("Submit")
+    }
+
+    const validations = (e: React.SyntheticEvent<EventTarget>) => {
+        setErrorMessage("")
+        e.preventDefault();
+        if (!name) {
+            setErrorMessage("Name field is empty!")
+        } else if (!email) {
+            setErrorMessage("Email field is empty!")
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+            setErrorMessage("Invalid email address!")
+        } else if (!text) {
+            setErrorMessage("Message field is empty")
+        } else { submitForm(e) }
+    }
+
+    const submitForm = (e: React.SyntheticEvent<EventTarget>) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_5qwlgkk', 'template_3z9yxwg', form.current, 'Y89xX9XApZV8jADya')
+        setFormStatus("Send...")
+        emailjs.sendForm('service_5qwlgkk', 'template_3z9yxwg', refOfForm.current, 'Y89xX9XApZV8jADya')
             .then((result) => {
-                alert("Will be send");
+                setFormStatus("Done");
+                resetFormValue()
+                setTimeout(changeFormStatusTimeOut, 2000)
+
             }, (error) => {
-                alert(error.text);
+                console.log(error.text)
+                setErrorMessage("SOMETHING WRONG WITH SERVER")
+                setFormStatus("Submit")
             });
+
     };
 
     return (
-        <form ref={form} className={styles.Form} onSubmit={sendEmail} >
-            <input className={styles.Input} placeholder="Name" type="text" name="user_name" autoComplete="name" />
-            <input className={styles.Input} placeholder="Email" type="text" name="user_email" autoComplete="email" />
-            <textarea className={styles.TextArea} placeholder="Message" name="message" id="" />
-            <button className={styles.Button} type="submit">Submit</button>
+        <form ref={refOfForm} className={styles.Form} onSubmit={validations}  >
+            <div>{errorMessage}</div>
+            <input className={styles.Input} placeholder="Name" type="text" name="user_name" autoComplete="name" value={name} onChange={(e) => setName(e.target.value)} />
+            <input className={styles.Input} placeholder="Email" type="text" name="user_email" autoComplete="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <textarea className={styles.TextArea} placeholder="Message" name="message" value={text} onChange={(e) => setText(e.target.value)} />
+            <button className={styles.Button} type="submit" >{formStatus}</button>
         </form>
     )
 }
